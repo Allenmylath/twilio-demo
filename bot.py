d19253f4134e2716598a4e663a8b94938f678a6f
allenmylath <allengeorgemylath@gmail.com>
2025-03-23 21:52:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -59,8 +59,7 @@ async def run_bot(websocket_client: WebSocket, stream_sid: str,):
     tts = CartesiaTTSService(
         api_key=os.getenv("CARTESIA_API_KEY"),
         voice_id="71a7ad14-091c-4e8e-a314-022ece01c121",  # British Reading Lady
-        push_silence_after_stop=testing,
-    )
+          )
 
     messages = [
         {
