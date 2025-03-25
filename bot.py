f9744a88143f0a647dc84d1be448682dad7106dd
allenmylath <allengeorgemylath@gmail.com>
2025-03-25 18:41:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -91,8 +91,8 @@ async def run_bot(websocket_client: WebSocket, stream_sid: str,):
     task = PipelineTask(
         pipeline,
         params=PipelineParams(
-            audio_in_sample_rate=8000,
-            audio_out_sample_rate=8000,
+            #audio_in_sample_rate=8000,
+            #audio_out_sample_rate=8000,
             allow_interruptions=True,
         ),
     )
